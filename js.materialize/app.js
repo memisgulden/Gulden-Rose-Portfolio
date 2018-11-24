c57a9ad2fab9b57e2f9ff0b9c6a0aef03c82f722
memisgulden <memis@gmail.com>
2018-11-24 12:48:26 -0500

    more stylin

---
+++ js.materialize/app.js
@@ -0,0 +1,19 @@
+// $("a").hover(function () {
+//     //on hover
+//     $(this).css("font-style", "italic"),   
+//     },
+//     // after hover
+//     function () {
+//         $(this).css("font-style", "none");
+//     });
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
+
+
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+  });
+        
